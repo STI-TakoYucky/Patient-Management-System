53e9cab048adc60cb0e4b8ef812e0f02ecb8fbae
Lucky Estrada <estrada.lnp@gmail.com>
2024-11-16 23:40:56 +0800

    First Commit

---
+++ src/mvc/views/App.java
@@ -0,0 +1,12 @@
+package mvc.views;
+
+import javax.swing.*;
+
+public class App extends JFrame {
+    public App() {
+        setVisible(true);
+        setSize(600, 600);
+        setTitle("PatientManagementSystem");
+        setLocationRelativeTo(null);
+    }
+}
