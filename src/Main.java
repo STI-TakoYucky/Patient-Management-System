e13bfc8eb209a38bc208e75571b96b7156e44996
Lucky Estrada <estrada.lnp@gmail.com>
2024-11-17 00:05:56 +0800

    testfix

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import mvc.views.App;
 public class Main {
     public static void main(String[] args) {
         new App();
-        System.out.println("Test Pull req);
+        System.out.println("Test Pull req");
     }
 }
